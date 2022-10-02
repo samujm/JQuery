1f051b64a61d55dd4ee11f8ca8e22e981d99fbac
Samantha González <samujm76@gmail.com>
2022-10-02 14:42:46 -0500

    Comentar para seguir la practica

--- app.js
+++ app.js
@@ -22,26 +22,28 @@ $(document).ready(function(){
     //$('.container h1:last').html('cambio de texto');
 
     //Agregar clases
-    $('#idh1').addClass('text-danger');
+    //$('#idh1').addClass('text-danger');
     //Quitar clase
-    $('#idh1').removeClass('display-4');
+    //$('#idh1').removeClass('display-4');
 
-    $('#contenido').append('<h1>Este es un elemento al final</h1>');
-    $('#contenido').prepend('<h1>Este es un elemento al principio</h1>');
+    // $('#contenido').append('<h1>Este es un elemento al final</h1>');
+    // $('#contenido').prepend('<h1>Este es un elemento al principio</h1>');
 
     //propiedad css
-    $('#color-azul').css('color', 'blue');
+    // $('#color-azul').css('color', 'blue');
     //Colocar más propiedades css al mismo tiempo
-    $('#color-azul').css({color:'blue', background: 'salmon', padding: '20px'});
+    // $('#color-azul').css({color:'blue', background: 'salmon', padding: '20px'});
 
     //Con remove desaparece completamente el elemento
-    $('#color-azul').remove();
+    // $('#color-azul').remove();
     //Con hide no aparece en la pagina pero si en html, con la propiedad none
-    $('#color-azul').hide();
+    // $('#color-azul').hide();
 
     //atributos 
-    $('img').attr('src', 'https://josefacchin.com/wp-content/uploads/2020/02/como-quitar-el-fondo-de-una-imagen.png');
-    $('img:last').attr('width','50');
+    // $('img').attr('src', 'https://josefacchin.com/wp-content/uploads/2020/02/como-quitar-el-fondo-de-una-imagen.png');
+    // $('img:last').attr('width','50');
+
+
 
 
 });
