7e8780f4f9957b8c9a9521d5d02f2fd595a10559
Samantha González <samujm76@gmail.com>
2022-10-01 22:06:33 -0500

    Agregar o quitar clases

--- app.js
+++ app.js
@@ -5,19 +5,24 @@ $(document).ready(function(){
 
     //---------------CON JQUERY
     //Seleccionar elementos a traves de etiquetas
-    $('h1').html('Etiqueta h1');
+    //$('h1').html('Etiqueta h1');
     //Seleccionar elementos a traves de clases
-    $('.display-4').html('desde clase');
+    //$('.display-4').html('desde clase');
     //Seleccionar elementos a traves de id
-    $('#idh1').html('desde id');
+    //$('#idh1').html('desde id');
 
     //---------------CON JS
-    document.querySelector('h1').innerHTML = 'Etiqueta h1';
-    document.querySelector('.display-4').innerHTML = 'desde clase';
-    document.querySelector('#idh1').innerHTML = 'desde id';
+    //document.querySelector('h1').innerHTML = 'Etiqueta h1';
+    //document.querySelector('.display-4').innerHTML = 'desde clase';
+    //document.querySelector('#idh1').innerHTML = 'desde id';
 
     //Seleccionando todos los h1 que estan dentro de container
     //$('.container h1').html('cambio de texto');
     //$('.container h1:first').html('cambio de texto');
     //$('.container h1:last').html('cambio de texto');
+
+    //Agregar clases
+    $('#idh1').addClass('text-danger');
+    //Quitar clase
+    $('#idh1').removeClass('display-4');
 });
