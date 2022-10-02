9d766619a1216c6992d3d6d4478cd651806c98e4
Samantha González <samujm76@gmail.com>
2022-10-02 14:51:18 -0500

    eventos, click con jquery

--- app.js
+++ app.js
@@ -44,6 +44,20 @@ $(document).ready(function(){
     // $('img:last').attr('width','50');
 
 
+    //---------------EVENTOS CON JQUERY
 
+    var parrafo = $('#resultado p');
+
+    $('.btn-primary').click(function(){
+        parrafo.addClass('display-4');
+    });
+
+    $('.btn-danger').click(function(){
+        parrafo.removeClass('display-4');
+    });
+
+    $('.btn-warning').click(function(){
+        parrafo.toggleClass('display-4')
+    });
 
 });
